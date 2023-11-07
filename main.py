f74bd861059e17063926687e18e96f5213c3e85e
George Marsh <georgemarsh@smmacademy.org>
2023-11-07 22:51:04 +0000

    mouse changes

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ class WashingtonBullets(ShowBase):
         ShowBase.__init__(self)
         self.properties()
         self.enviroment1()
+        self.mouseInGame()
+        self.controls()
 
-    def properties(self):
+
+        taskMgr.add(self.update, 'update')
+
+
+    def properties(self): #sets up game properties
         props = WindowProperties() #creates window properties
         props.setTitle('Washington Bullets') #name of app running
         props.setSize(1280, 720 ) #sets display size
-        '''cursor settings''' #in properties as cursor settings use WindowProps, ease of use
-        props.setCursorHidden(True) #hides cursor
-        props.setMouseMode(WindowProperties.M_relative) #holds mouse in place
         base.win.requestProperties(props)
 
     def enviroment1(self):
@@ -21,16 +24,28 @@ class WashingtonBullets(ShowBase):
         self.scene.reparentTo(self.render)
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(0, -59, 0)
-        
+
+    def controls(self):
+        self.disableMouse()
+        self.accept('mouse1', self.mouseInGame)
+        self.accept('escape', self.mouseRelease)
     
+    def mouseInGame(self): #takes mouse input in game
+        props = WindowProperties()
+        props.setCursorHidden(True) #hides cursor
+        props.setMouseMode(WindowProperties.M_relative) #holds mouse in place in game
+        self.win.requestProperties(props)
+    
+    def mouseRelease(self):
+        props = WindowProperties()
+        props.setCursorHidden(False) #shows cursor
+        props.setMouseMode(WindowProperties.M_absolute) #holds mouse can go outside window
+        self.win.requestProperties(props)
 
-
-
+    def update(self, logic)
         
-
-
-
-        
+        return logic.cont
+    
 
 
 app = WashingtonBullets()
