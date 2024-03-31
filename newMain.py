0a88d366862192733cbe8e55ff7399f419e4deaf
georgemarshsmma <148215719+georgemarshsmma@users.noreply.github.com>
2024-03-31 20:05:11 +0100

    Update newMain.py

--- newMain.py
+++ newMain.py
@@ -1,84 +1,116 @@
 from direct.showbase.ShowBase import ShowBase 
 from panda3d.core import WindowProperties 
-from panda3d.core import CollisionTraverser, CollisionSphere, CollisionNode, CollisionBox, CollisionHandlerQueue, CollisionHandlerPusher, CollisionRay
-from panda3d.core import GraphicsWindow
+from panda3d.core import CollisionTraverser, CollisionSphere, CollisionNode, CollisionHandlerQueue, CollisionHandlerPusher, CollisionRay
 from panda3d.core import NodePath
 from panda3d.core import BitMask32
-from panda3d.core import Vec3, Vec2
-from panda3d.core import loadPrcFile
+from panda3d.core import Vec3
 from direct.gui.OnscreenImage import OnscreenImage
+from panda3d.core import TransparencyAttrib
+from panda3d.core import Point3
+from panda3d.core import TextNode
 import sys
 from direct.actor.Actor import Actor
 from direct.gui.DirectGui import *
-from direct.showbase.DirectObject import DirectObject
-from direct.interval.IntervalGlobal import Sequence
-from direct.interval.FunctionInterval import Func
-from panda3d.bullet import BulletWorld, BulletSphereShape, BulletRigidBodyNode, BulletDebugNode
 import random
 
 class game(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+        self.collision()
         self.properties()
         self.initMenu()
-        self.collision()
 
     def collision(self):
         base.cTrav = CollisionTraverser()
         base.pusher = CollisionHandlerPusher()
 
-
-    def properties(self): #sets up game properties
-        props = WindowProperties() #creates window properties
-        props.setTitle('game') #name of app running
-        props.setSize(800, 600) #sets display size
-        base.win.requestProperties(props)
+    def properties(self):
         base.disableMouse()
-    
+        props = WindowProperties()
+        props.setTitle('game')
+        props.setSize(800, 600)
+        base.win.requestProperties(props)
+
     def initMenu(self):
         self.startMenu()
-    
-    def startGame(self):
-        print('starting game')
-        self.destroyMenu()
-        self.initEnvironment()
-        self.initActor()
-        self.initEnemy()
-    
+
     def exitGame(self):
         print('exiting game')
         self.destroy()
-    
+
     def startMenu(self):
-        self.start_frame = DirectFrame(frameSize=(-1, 1, -1, 1), frameColor=(0, 0, 0, 0.5))
-        self.start_frame.setPos(0, 0, 0)
+        self.startFrame = DirectFrame(frameSize=(-1, 1, -1, 1), frameColor=(0, 0, 0, 0.5))
+        self.startFrame.setPos(0, 0, 0)
 
-        start_button = DirectButton(text="Start Game", scale=0.1, command=self.startGame)
-        start_button.setPos(0, 0, 0.2)
-        start_button.reparentTo(self.start_frame)
+        map1Button = DirectButton(text='Map 1', scale=0.1, command=self.startGameMap1)
+        map1Button.setPos(0, 0, 0.4)
+        map1Button.reparentTo(self.startFrame)
 
-        exit_button = DirectButton(text="Exit", scale=0.1, command=self.exitGame)
-        exit_button.setPos(0, 0, -0.2)
-        exit_button.reparentTo(self.start_frame)
-    
+        map2Button = DirectButton(text='Map 2', scale=0.1, command=self.startGameMap2)
+        map2Button.setPos(0, 0, 0)
+        map2Button.reparentTo(self.startFrame)
+
+        map3Button = DirectButton(text='Map 3', scale=0.1, command=self.startGameMap3)
+        map3Button.setPos(0, 0, -0.4)
+        map3Button.reparentTo(self.startFrame)
+
+        exitButton = DirectButton(text='Exit', scale=0.1, command=self.exitGame)
+        exitButton.setPos(0, 0, -0.8)
+        exitButton.reparentTo(self.startFrame)
+
     def destroyMenu(self):
-        self.start_frame.destroy()
+        self.startFrame.destroy()
 
-    def initEnvironment(self):
+    def initEnvironment1(self):
         self.environment1()
+    
+    def initEnvironment2(self):
+        self.environment2()
+    
+    def initEnvironment3(self):
+        self.environment3()
 
     def environment1(self):
         self.scene = self.loader.loadModel('environment/OldWestTerrain/OldWestTerrain.egg')
-        #self.scene.setScale(1, 1, 1)
         self.scene.reparentTo(self.render)
         self.scene.setTwoSided(True)
+    
+    def environment2(self):
+        self.scene = self.loader.loadModel('environment/Cafeteria/Cafeteria.egg')
+        self.scene.setScale(0.06, 0.06, 0.06)
+        self.scene.reparentTo(self.render)
+        self.scene.setTwoSided(True)
+    
+    def environment3(self):
+        pass
 
     def initActor(self):
         self.actor = Player()
-    
+
     def initEnemy(self):
         self.enemy = Enemy(self.actor)
+
+    def startGameMap1(self):
+        print('starting game - Map 1')
+        self.destroyMenu()
+        self.initEnvironment1()
+        self.initActor()
+        self.initEnemy()
+
+    def startGameMap2(self):
+        print('starting game - Map 2')
+        self.destroyMenu()
+        self.initEnvironment2()
+        self.initActor()
+        self.initEnemy()
+
+    def startGameMap3(self):
+        print('starting game - Map 3')
+        self.destroyMenu()
+        self.initEnvironment3()
+        self.initActor()
+        self.initEnemy()
 
     
 class Player(Actor):
@@ -91,8 +123,8 @@ class Player(Actor):
     STOP = Vec3(0)
     walk = STOP
     strafe = STOP
-    readyToJump = False
-    jump = 0
+    GRAVITY = 9.8
+    health = 100
 
     
     def __init__(self):
@@ -100,56 +132,101 @@ class Player(Actor):
         self.setupCamera()
         self.collision()
         self.controls()
-        #health - initial health, items in game that increase health,
-        #weapon = Weapon(), argument dictates how strong the weapon is,
+        self.properties()
+        self.displayHealth()
+        self.velocity = Vec3(0,0,0)
 
         taskMgr.add(self.mouseTask, 'mouse-update')
         taskMgr.add(self.moveTask, 'move-update')
-        taskMgr.add(self.jumpTask, 'jump-update')
-    
+        taskMgr.add(self.gravityTask, 'gravity')
+        
     def loadModel(self):
         self.player = NodePath('player')
         self.player.reparentTo(render)
         self.player.setPos(0, 10, 2)
 
     def setupCamera(self):
-        pl = base.cam.node().getLens()
-        pl.setFov(70)
-        base.cam.node().setLens(pl)
+        playerView = base.cam.node().getLens()
+        playerView.setFov(70)
+        base.cam.node().setLens(playerView)
         base.camera.reparentTo(self.player)
+
+        crosshairs = OnscreenImage(image = 'UI/crosshairs.png', pos = (0,0,0), scale = 0.1)
+        crosshairs.setTransparency(TransparencyAttrib.MAlpha)
     
+    def displayHealth(self):
+        self.healthText = OnscreenText(text = "Health: {}".format(self.health),
+                                       pos = (-1.3, 0.9), 
+                                       scale = 0.1, 
+                                       fg=(1, 1, 1, 1),
+                                       align=TextNode.ALeft)
+        self.healthText.setTransparency(TransparencyAttrib.MAlpha)
+
+    def updateHealth(self):
+        self.health += -20
+        self.healthText.setText("Health: {}".format(self.health))
+    
+    def properties(self):
+        base.disableMouse()
+        props = WindowProperties()
+        props.setCursorHidden(True)
+        base.win.requestProperties(props)
+        
     def collision(self):
         colliderNode = CollisionNode('player')
-        colliderNode.addSolid(CollisionSphere(0,0,0,4))
+        colliderNode.addSolid(CollisionSphere(0,0,0,5))
         solid = self.player.attachNewNode(colliderNode)
         base.cTrav.addCollider(solid, base.pusher)
         base.pusher.addCollider(solid, self.player, base.drive.node())
 
-        ray = CollisionRay()
-        ray.setOrigin(0,0,-.2)
-        ray.setDirection(0,0,-1)
+        ray = CollisionRay(0,0,0,0,0,-1)
         colliderNode = CollisionNode('playerRay')
         colliderNode.addSolid(ray)
-        colliderNode.setFromCollideMask(BitMask32.bit(1)) 
+        colliderNode.setFromCollideMask(BitMask32.bit(1))
         colliderNode.setIntoCollideMask(BitMask32.allOff())
         solid = self.player.attachNewNode(colliderNode)
         self.nodeGroundHandler = CollisionHandlerQueue()
         base.cTrav.addCollider(solid, self.nodeGroundHandler)
 
-    
+        
     def controls(self):
         self.accept('escape', sys.exit)
-        self.accept('space', self.__setattr__, ['readyToJump', True])
-        self.accept('space-up', self.__setattr__, ['readyToJump', False])
-        self.accept( "w" , self.__setattr__,["walk",self.FORWARD])
-        self.accept( "s" , self.__setattr__,["walk",self.BACK] )
-        self.accept( "s-up" , self.__setattr__,["walk",self.STOP] )
-        self.accept( "w-up" , self.__setattr__,["walk",self.STOP] )
-        self.accept( "a" , self.__setattr__,["strafe",self.LEFT])
-        self.accept( "d" , self.__setattr__,["strafe",self.RIGHT] )
-        self.accept( "a-up" , self.__setattr__,["strafe",self.STOP] )
-        self.accept( "d-up" , self.__setattr__,["strafe",self.STOP] )
-            
+        self.accept('w' , self.__setattr__,['walk',self.FORWARD])
+        self.accept('s' , self.__setattr__,['walk',self.BACK])
+        self.accept('s-up' , self.__setattr__,['walk',self.STOP])
+        self.accept('w-up' , self.__setattr__,['walk',self.STOP])
+        self.accept( 'a' , self.__setattr__,['strafe',self.LEFT])
+        self.accept( 'd' , self.__setattr__,['strafe',self.RIGHT])
+        self.accept( 'a-up' , self.__setattr__,['strafe',self.STOP])
+        self.accept( 'd-up' , self.__setattr__,['strafe',self.STOP])
+        self.accept('mouse1', self.shoot)
+    
+
+    def shoot(self):
+        if base.mouseWatcherNode.hasMouse():
+            mousePos = base.mouseWatcherNode.getMouse()
+            nearPoint = Point3()
+            farPoint = Point3()
+            base.camLens.extrude(mousePos, nearPoint, farPoint)
+            picker = CollisionTraverser()
+            queue = CollisionHandlerQueue()
+            pickerRay = CollisionRay()
+            pickerNode = CollisionNode('mouseRay')
+            pickerNode.setFromCollideMask(BitMask32.bit(1))
+            pickerNode.addSolid(pickerRay)
+            pickerNP = base.camera.attachNewNode(pickerNode)
+            pickerNode.setFromCollideMask(BitMask32.bit(0)) 
+            picker.addCollider(pickerNP, queue)
+            picker.traverse(render)
+            if queue.getNumEntries() > 0:
+                queue.sortEntries()
+                pickedObj = queue.getEntry(0).getIntoNodePath()
+                if pickedObj.hasPythonTag("enemy"):
+                    enemy = pickedObj.getPythonTag("enemy")
+                    enemy.cleanup()
+                    return
+
+
     def mouseTask(self, task):
         md = base.win.getPointer(0)
         x = md.getX()
@@ -159,88 +236,65 @@ class Player(Actor):
             base.camera.setP(base.camera.getP() - (y - base.win.getYSize()/2)*0.1)
         return task.cont
     
+
     def moveTask(self,task):
         self.player.setPos(self.player,self.walk*globalClock.getDt()*self.speed)
         self.player.setPos(self.player,self.strafe*globalClock.getDt()*self.speed)
         return task.cont
     
-    def jumpTask(self, task):
-        # get the highest Z from the down casting ray
-        highestZ = -100
-        for i in range(self.nodeGroundHandler.getNumEntries()):
-            entry = self.nodeGroundHandler.getEntry(i)
-            z = entry.getSurfacePoint(render).getZ()
-            if z > highestZ and entry.getIntoNode().getName() == "Cube":
-                highestZ = z
-        # gravity effects and jumps
-        self.player.setZ(self.player.getZ()+self.jump*globalClock.getDt())
-        self.jump -= 6*globalClock.getDt()
-        if highestZ > self.player.getZ()-10:
-            self.jump = 0
-            self.player.setZ(highestZ+.3)
-            if self.readyToJump:
-                self.jump = 1
+
+    def gravityTask(self,task):
+        self.velocity.z -= self.GRAVITY * globalClock.getDt()
+        self.player.setPos(self.player, self.velocity*globalClock.getDt()*self.speed)
+        if self.player.getZ() < 10:
+            self.player.setZ(2)
+            self.velocity.z = 0
         return task.cont
 
-
+    
 class Enemy(Actor):
     
     def __init__(self, player):
         self.player = player
         self.enemy = Actor('models/panda-model.egg',
                            {'walk': 'models/panda-walk4'})
-        self.enemy.setScale(0.009)
+        self.enemy.setScale(0.009,0.009,0.009)
         pos = random.randint(-100,100)
         self.enemy.setPos(pos, pos, 1)
+        self.enemy.setZ(2)
         self.enemy.reparentTo(render)
+        self.enemy.setPythonTag('enemy','target')
         self.enemy.loop('walk')
         self.followPlayer()
+        self.collision()
     
     def followPlayer(self):
-        taskMgr.add(self.updateFollow, 'update follow', extraArgs=[self.player], appendTask=True)
-
+        taskMgr.add(self.updateFollow, 'update-follow' , extraArgs=[self.player], appendTask=True)
+    
+    def cleanup(self):
+        taskMgr.remove('update-follow')
+    
+    def collision(self):
+        colliderNode = CollisionNode('enemy')
+        colliderNode.addSolid(CollisionSphere(0,0,0,5))
+        solid = self.enemy.attachNewNode(colliderNode)
+        base.cTrav.addCollider(solid, base.pusher)
+        base.pusher.addCollider(solid, self.enemy, base.drive.node())
 
     def updateFollow(self, player, task):
-        directionToPlayer = player.player.getPos() - self.enemy.getPos()
-        directionToPlayer.setZ(0)
-        directionToPlayer.normalize()
-        self.enemy.setPos(self.enemy.getPos() + directionToPlayer * 0.1)
+        distanceToPlayer = player.player.getPos() - self.enemy.getPos()
+        distanceToPlayer.setZ(0)
+        distanceToPlayer.normalize()
+        self.enemy.setPos(self.enemy.getPos() + distanceToPlayer * 0.4)
         self.enemy.lookAt(player.player)
         return task.cont
-
-
-class Weapon(DirectObject):
-
-    def __init__(self, render, player, enemyList):
-        self.render = render
-        self.player = player
-        self.enemyList = enemyList
-
-        self.accept('mouse1', self.fire)
-
     
-    def fire(self):
-        bullet = Bullet(self.render, self.player.getPos(), 'models/bullet.glb')
-        bullet.move()
-        bullet.detectCollision(self.enemyList)
-
-class Bullet():
-    def __init__(self, render, startPos, modelPath):
-        self.render = render
-        self.bullet = loader.loadModel(modelPath)
-        self.bullet.setPos(startPos)
-        self.bullet.reparentTo(self.render)
+    def updateHealthEnemy(self, player, task):
+        distance = self.enemy.getDistance(player.player)
+        if distance < 5:
+            player.updateHealth()  
+        return task.cont
     
-    def move(self):
-        bullet_interval = self.bullet.posInterval(1, Vec3(10, 0, 0), startPos=self.bullet.getPos())
-        bullet_sequence = Sequence(bullet_interval, Func(self.bullet.removeNode))
-        bullet_sequence.start()
-
-    def detectCollision(self, enemyList):
-        for enemy in enemyList:
-            if self.bullet.getPos(render).getX() == enemy.getPos(render).getX() and self.bullet.getPos(render).getY == enemy.getPos(render).getY():
-                enemy.removeNode()
-
 
 app = game()
 app.run()
